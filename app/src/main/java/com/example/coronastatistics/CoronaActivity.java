6516911874c8ad207399f39bdbf93b876ede25d4
Nguyen Tien Dung Otten <nguyen.tiendung.otten@gmail.com>
2020-03-23 03:32:44 +0100

    - App stellt beim Öffnen Verbindung zum Corona Endpoint her - Asynchrone Connection - Lädt die Stats in den Arbeitsspeicher - Noch fixe Parametereingabe

--- app/src/main/java/com/example/coronastatistics/CoronaActivity.java
+++ app/src/main/java/com/example/coronastatistics/CoronaActivity.java
@@ -6,19 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class CoronaStats extends AppCompatActivity {
+import java.io.IOException;
+
+public class CoronaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_corona_stats);
+        setContentView(R.layout.activity_corona);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.editText);
         textView.setText(message);
     }
 }
